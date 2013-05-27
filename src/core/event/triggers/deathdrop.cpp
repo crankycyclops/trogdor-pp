8397421218605a0fcf3dcf06f6456a699af502aa
James Colannino <crankycyclops@gmail.com>
2013-05-27 16:17:26 -0700

    fixed a bug, where in deathdrop event handler I would get a segfault if I let drops trigger events

--- src/core/event/triggers/deathdrop.cpp
+++ src/core/event/triggers/deathdrop.cpp
@@ -9,11 +9,17 @@ namespace core { namespace event {
 
       Being *being = static_cast<Being *>(boost::get<Entity *>(args[1]));
 
+      entity::ObjectList drops;
+
       // TODO: should we only drop with some probability?
       // TODO: right now, we drop undroppable objects; should we?
       entity::ObjectSetCItPair invItems = being->getInventoryObjects();
       for (ObjectSetCIt i = invItems.begin; i != invItems.end; i++) {
-         being->drop(*i, false, false);
+         drops.push_back(*i);
+      }
+
+      for (entity::ObjectListCIt i = drops.begin(); i != drops.end(); i++) {
+         being->drop(*i, false);
       }
 
       continueExecutionFlag = true;
