cd65d411f6b26e7d58c0e306898440692a10b7b5
James Colannino <crankycyclops@gmail.com>
2013-10-14 22:43:39 +0000

    #38: added LuaBeing barebones

---
+++ src/core/include/lua/api/entities/luabeing.h
@@ -0,0 +1,72 @@
+#ifndef LUABEING_H
+#define LUABEING_H
+
+
+#include "../../luatable.h"
+#include "../../luastate.h"
+
+#include "luathing.h"
+
+
+using namespace std;
+
+namespace core { namespace entity {
+
+
+   class Being;
+
+
+   class LuaBeing: public LuaThing {
+
+
+      protected:
+
+         /*
+            Checks that a Being exists at the specified location on the Lua
+            stack, and returns it if it does.
+
+            Input:
+               Lua State
+               Index on stack
+
+            Output:
+               Being * (or 0 if type doesn't match or doesn't exist)
+         */
+         static Being *checkBeing(lua_State *L, int i);
+
+         /*
+            Returns all functions to be registered to our Lua wrapper around
+            Being.
+
+            Input:
+               (none)
+
+            Output:
+               Methods (luaL_reg[])
+         */
+         static const luaL_reg *getFunctions();
+
+         /*
+            Returns all methods to be registered to our Lua wrapper around
+            Being.
+
+            Input:
+               (none)
+
+            Output:
+               Methods (luaL_reg[])
+         */
+         static const luaL_reg *getMethods();
+
+      public:
+
+         /*
+            Registers the Being type and its associated operations in Lua.
+         */
+         static void registerLuaType(lua_State *L);
+   };
+}}
+
+
+#endif
+
---
+++ src/core/lua/api/entities/luabeing.cpp
@@ -0,0 +1,62 @@
+#include "../../../include/entities/being.h"
+#include "../../../include/lua/api/entities/luabeing.h"
+
+using namespace std;
+
+namespace core { namespace entity {
+
+
+   // Types which are considered valid by checkBeing()
+   static const char *beingTypes[] = {
+      "Being",
+      "Player",
+      "Creature",
+      0
+   };
+
+   // functions that take a Being as an input (new, get, etc.)
+   // format of call: Being.new(e), where e is a Being
+   static const luaL_reg functions[] = {
+      {0, 0}
+   };
+
+   // Lua Being methods that bind to C++ Being methods
+   // also includes meta methods
+   static const luaL_reg methods[] = {
+      {0, 0}
+   };
+
+
+   const luaL_reg *LuaBeing::getFunctions() {
+
+      return functions;
+   }
+
+   const luaL_reg *LuaBeing::getMethods() {
+
+      return methods;
+   }
+
+   void LuaBeing::registerLuaType(lua_State *L) {
+
+      luaL_newmetatable(L, "Being");
+
+      // Being.__index = Being
+      lua_pushvalue(L, -1);
+      lua_setfield(L, -2, "__index");
+
+      luaL_register(L, 0, LuaEntity::getMethods());
+      luaL_register(L, 0, LuaThing::getMethods());
+      luaL_register(L, 0, methods);
+      luaL_register(L, "Being", LuaEntity::getFunctions());
+      luaL_register(L, "Being", LuaThing::getFunctions());
+      luaL_register(L, "Being", functions);
+   }
+
+   Being *LuaBeing::checkBeing(lua_State *L, int i) {
+
+      luaL_checktype(L, i, LUA_TUSERDATA);
+      return *(Being **)LuaState::luaL_checkudata_ex(L, i, beingTypes);
+   }
+}}
+
