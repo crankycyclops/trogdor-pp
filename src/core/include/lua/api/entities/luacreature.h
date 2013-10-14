986756c2ae731d2ee9ea66120c6b2eab00d727e0
James Colannino <crankycyclops@gmail.com>
2013-10-14 22:50:26 +0000

    #38: added LuaCreature bare bones

---
+++ src/core/include/lua/api/entities/luacreature.h
@@ -0,0 +1,72 @@
+#ifndef LUACREATURE_H
+#define LUACREATURE_H
+
+
+#include "../../luatable.h"
+#include "../../luastate.h"
+
+#include "luabeing.h"
+
+
+using namespace std;
+
+namespace core { namespace entity {
+
+
+   class Creature;
+
+
+   class LuaCreature: public LuaBeing {
+
+
+      protected:
+
+         /*
+            Checks that a Creature exists at the specified location on the Lua
+            stack, and returns it if it does.
+
+            Input:
+               Lua State
+               Index on stack
+
+            Output:
+               Creature * (or 0 if type doesn't match or doesn't exist)
+         */
+         static Creature *checkCreature(lua_State *L, int i);
+
+         /*
+            Returns all functions to be registered to our Lua wrapper around
+            Creature.
+
+            Input:
+               (none)
+
+            Output:
+               Methods (luaL_reg[])
+         */
+         static const luaL_reg *getFunctions();
+
+         /*
+            Returns all methods to be registered to our Lua wrapper around
+            Creature.
+
+            Input:
+               (none)
+
+            Output:
+               Methods (luaL_reg[])
+         */
+         static const luaL_reg *getMethods();
+
+      public:
+
+         /*
+            Registers the Creature type and its associated operations in Lua.
+         */
+         static void registerLuaType(lua_State *L);
+   };
+}}
+
+
+#endif
+
---
+++ src/core/lua/api/entities/luacreature.cpp
@@ -0,0 +1,62 @@
+#include "../../../include/entities/creature.h"
+#include "../../../include/lua/api/entities/luacreature.h"
+
+using namespace std;
+
+namespace core { namespace entity {
+
+
+   // Types which are considered valid by checkCreature()
+   static const char *creatureTypes[] = {
+      "Creature",
+      0
+   };
+
+   // functions that take a Creature as an input (new, get, etc.)
+   // format of call: Creature.new(e), where e is a Creature
+   static const luaL_reg functions[] = {
+      {0, 0}
+   };
+
+   // Lua Creature methods that bind to C++ Creature methods
+   // also includes meta methods
+   static const luaL_reg methods[] = {
+      {0, 0}
+   };
+
+
+   const luaL_reg *LuaCreature::getFunctions() {
+
+      return functions;
+   }
+
+   const luaL_reg *LuaCreature::getMethods() {
+
+      return methods;
+   }
+
+   void LuaCreature::registerLuaType(lua_State *L) {
+
+      luaL_newmetatable(L, "Creature");
+
+      // Creature.__index = Creature
+      lua_pushvalue(L, -1);
+      lua_setfield(L, -2, "__index");
+
+      luaL_register(L, 0, LuaEntity::getMethods());
+      luaL_register(L, 0, LuaThing::getMethods());
+      luaL_register(L, 0, LuaBeing::getMethods());
+      luaL_register(L, 0, methods);
+      luaL_register(L, "Creature", LuaEntity::getFunctions());
+      luaL_register(L, "Creature", LuaThing::getFunctions());
+      luaL_register(L, "Creature", LuaBeing::getFunctions());
+      luaL_register(L, "Creature", functions);
+   }
+
+   Creature *LuaCreature::checkCreature(lua_State *L, int i) {
+
+      luaL_checktype(L, i, LUA_TUSERDATA);
+      return *(Creature **)LuaState::luaL_checkudata_ex(L, i, creatureTypes);
+   }
+}}
+
