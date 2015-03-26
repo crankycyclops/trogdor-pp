018bca957585d2114c3d8617572fd67a685c2f84
James Colannino <crankycyclops@gmail.com>
2015-03-25 23:34:21 -0700

    added a slightly modified game.xml optimized for a multiplayer server environment and FINALLY hooked the server into the core!

--- src/online/server/main.cpp
+++ src/online/server/main.cpp
@@ -1,13 +1,47 @@
+#include <iostream>
+#include <cstdlib>
 #include <boost/asio.hpp>
 
 #include "include/network/tcpcommon.h"
 #include "include/network/tcpconnection.h"
 #include "include/network/tcpserver.h"
 
+#include "../../core/include/game.h"
+#include "../../core/include/iostream/streamout.h"
+#include "../../core/include/iostream/streamin.h"
+
 #define SERVER_PORT 1040 // config that should be moved outside
+
+using namespace std;
+
+
+// global object that should be accessible to the entire application
+core::Game *currentGame;
 
 
 int main(int argc, char **argv) {
+
+	// default game filename
+	string gameXML = "game-server.xml";
+
+	if (argc > 2) {
+		cerr << "\nUsage: trogdor [game_filename.xml]\n\n" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// user passed in a custom game filename
+	if (argc > 1) {
+		gameXML = argv[1];
+	}
+
+	currentGame = new core::Game();
+
+	if (currentGame->initialize(gameXML)) {
+		currentGame->start();
+	} else {
+		cerr << "\nFailed to initialize the game :'(\n\n" << endl;
+		return EXIT_FAILURE;
+	}
 
 	boost::asio::io_service io;
 	TCPServer *server;
@@ -17,6 +51,7 @@ int main(int argc, char **argv) {
 	server = new TCPServer(io, SERVER_PORT);
 	io.run();
 
+	currentGame->stop();
 	delete server;
 	return 0;
 }
