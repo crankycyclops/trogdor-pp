1fd03500033e5c220fcef880a1cd958f3c699e1f
James Colannino <crankycyclops@gmail.com>
2015-03-28 16:26:00 -0700

    fixed bug in streambuf handling during async read callback

--- src/online/server/network/tcpconnection.cpp
+++ src/online/server/network/tcpconnection.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
@@ -22,8 +23,23 @@ void TCPConnection::handleRead(
 
 		// copy the contents of the buffer to a string, trim out EOT and clear
 		// the buffer for the next request
-		bufferStr = boost::asio::buffer_cast<const char *>(inBuffer.data());
-		bufferStr.erase(remove(bufferStr.begin(), bufferStr.end(), EOT), bufferStr.end());
+		const char *bufferData = boost::asio::buffer_cast<const char *>(inBuffer.data());
+
+		// A little hacky. Basically, every time new data is written into the
+		// streambuffer, the string inside isn't actually overwritten with '\0's.
+		// As a result, smaller strings that come in later, terminated in EOT,
+		// still contain the remainder of the previously received longer string,
+		// which of course produces weird undefined behavior. My workaround,
+		// whether or not it's the right way, is to replace EOT with '\0',
+		// which will result in the creation of a new valid C-style string.
+		char *p = (char *)bufferData;
+		for (; *p != EOT; p++);
+		*p = '\0';
+
+		// copy the corrected streambuf string into std::string bufferStr
+		bufferStr = bufferData;
+
+		// clearing the actual streambuf frees it up for later requests
 		clearBuffer();
 
 		// if a callback was passed, call it
