dc3d54ae8876ee23b21eb07aa7cf5da40de8eab0
James Colannino <crankycyclops@gmail.com>
2015-03-28 18:01:30 -0700

    tried to clearly comment on the server command workflow

--- src/online/server/include/command/actions/commandaction.h
+++ src/online/server/include/command/actions/commandaction.h
@@ -7,12 +7,44 @@
 #include "../../../../../core/include/thread.h"
 
 
-// Receives a player's command from the client and passes it to the game to be
-// processed.
+/*
+	Receives a player's command from the client and passes it to the game to be
+	processed.
+
+	Workflow:
+
+	1) execute() checks if the specified player is in the game. If he isn't,
+	   then we return NOEXIST. If it does, we spin off a separate thread that
+	   runs processCommandThread. We need to run this inside a separate thread
+	   because TCPIn will block while it waits to read data from the client.
+
+	2) inside processCommandThread, we retrieve the player, bind the TCPIn
+	   object to our current connection and run game->processCommand(player.)
+
+	3) game->processCommand(player) will read one or more lines of input from
+	   the user's client. To do this, we write to the client the command GET,
+	   instructing the client to send a raw string representing the command
+	   (or subsequently requested data.)
+
+	4) processCommand ultimately calls TCPIn's input operator, which blocks
+	   the thread on a mutex so that only one operation can be performed on
+	   any given TCPIn object at a time.
+
+	5) TCPIn writes out GET and waits for the client's response. It then sleeps
+	   (instead of blocking, so that we can continue to asynchronously accept
+	   requests in the background) until we get back the client's response,
+	   afterwhich we output the string from the stream operator and unlock
+	   the mutex.
+
+	6) The thread ends and we mark the connection as free for more requests.
+*/
 class COMMANDAction: public NetworkAction {
 
 	private:
 
+		// TODO: need one for each connection, since we recycle the CommandAction
+		// object. Do I want to use an unordered_map to map connections to
+		// individual commandThreads?
 		thread_t commandThread;
 
 		// Thread that processes a player command.
