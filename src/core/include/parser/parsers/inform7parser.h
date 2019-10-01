d4065904a55d0b5912d644fa342de0be2971ed88
James Colannino <crankycyclops@gmail.com>
2019-10-01 21:59:55 +0800

    added documentation to the inform 7 parser class and refactored it a little

--- src/core/include/parser/parsers/inform7parser.h
+++ src/core/include/parser/parsers/inform7parser.h
@@ -16,24 +16,49 @@ using namespace std;
 namespace trogdor {
 
    /*
-      Parses an Inform 7 source file.
+      Parses an Inform 7 source file and uses it to instantiate the game. This
+      is an experimental work in progress, and since Trogdor-pp and Inform are
+      different projects with different goals, this will likely never support
+      the entire language.
+
+      I'm writing this to learn more about grammars and parsing and because I
+      think it will be a fun project :) Don't expect this to be particularly
+      useful.
+
+      Here's the current subset of the language I'm parsing in EBNF format (I'll
+      keep this updated as support for additional language features grows):
+
+      <statement>            ::= <assertion>
+      <assertion>            ::= <definition> [<description>] | <adjective assignment>
+      <description>          ::= "\""/^[\"]+/".\""["."] | "\""/^[\"]+/["."]"\"."
+      <definition>           ::= ([<article>] <noun> "is" [<article>] [<adjective>] <class> | [<article>] <noun>{"," [<article>] <noun>}[","] "and" [<article>] <noun> "are" [<adjective>] <plural class>) ["in" [<article>] <noun>] | [<article>] <noun> "is" <direction> ("of" | "from") [<article>] <noun>
+      <adjective assignment> ::= [<article>] <noun> is <adjective> | [<article>] <noun>{"," [<article>] <noun>}[","] and [<article>] <noun> are <adjective>
+      <noun>                 ::= /[A-Za-z ']+/
+      <adjective>            ::= /[A-Za-z ]+/
+      <assertion verb>       ::= "has" | "carries" | "is carrying" | "wears" | "is wearing" | "contains" | "supports" | "is supporting"
+      <article>              ::= "a" | "an" | "the"
+      <direction>            ::= "north" | "northeast" | "east" | "southeast" | "south" | "southwest" | "west" | "northwest" | "up" | "down" | "inside" | "outside"
+      <class>                ::= "object" | "direction" | "room" | "region" | "thing" | "door" | "container" | "vehicle" | "player's holdall" | "supporter" | "backdrop" | "device" | "person" | "man" | "woman" | "animal"
+      <plural class>         ::= "objects" | "directions" | "rooms" | "regions" | "things" | "doors" | "containers" | "vehicles" | "player's holdalls" | "supporters" | "backdrops" | "devices" | "people" | "men" | "women" | "animals"
+
+      * Classes included above are those that are built into Inform 7. Once I
+      add support for custom classes, I'll also have to be able to parse those.
+      The same goes for custom directions, synonyms, etc.
+
+      Note that I had to cobble together the EBNF above myself using examples
+      from the official documentation along with other supplemental sources.
+
+      This one in particular helped a lot:
+      http://www.ifwiki.org/index.php/Inform_7_for_Programmers
    */
    class Inform7Parser: public Parser {
-
-      private:
-
-         // TODO
 
       public:
 
          /*
             Constructor
          */
-         inline Inform7Parser(std::unique_ptr<Instantiator> i, const Vocabulary &v):
-         Parser(std::move(i), v) {
-
-            // TODO
-         }
+         Inform7Parser(std::unique_ptr<Instantiator> i, const Vocabulary &v);
 
          /*
             Destructor
--- src/core/parser/parsers/inform7parser.cpp
+++ src/core/parser/parsers/inform7parser.cpp
@@ -1,12 +1,21 @@
 #include <memory>
 
 #include "../../include/utility.h"
+#include "../../include/vocabulary.h"
 
 #include "../../include/parser/parsers/inform7parser.h"
 #include "../../include/exception/parseexception.h"
 
 namespace trogdor {
 
+
+   Inform7Parser::Inform7Parser(std::unique_ptr<Instantiator> i,
+   const Vocabulary &v): Parser(std::move(i), v) {
+
+      // TODO
+   }
+
+   /**************************************************************************/
 
    Inform7Parser::~Inform7Parser() {
 
