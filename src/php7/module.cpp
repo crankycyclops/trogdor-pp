a516db1984df795b0632bdab6cf2b536d54cb6c1
James Colannino <crankycyclops@gmail.com>
2020-01-12 16:48:20 +0800

    added bare MSHUTDOWN and MINFO functions to PHP 7 module

--- src/php7/module.cpp
+++ src/php7/module.cpp
@@ -1,10 +1,26 @@
 // Look at C++ blog for example of how to allocate/free resources (constructor and destructor using object handlers)
 #include "module.h"
 
+PHP_MINFO_FUNCTION(trogdor) {
+
+	// TODO
+}
+
+/*****************************************************************************/
+
 PHP_MINIT_FUNCTION(trogdor) {
 
 	// TODO
 }
+
+/*****************************************************************************/
+
+PHP_MSHUTDOWN_FUNCTION(trogdor) {
+
+	// TODO
+}
+
+/*****************************************************************************/
 
 zend_module_entry trogdor_module_entry = {
 
@@ -13,12 +29,12 @@ zend_module_entry trogdor_module_entry = {
 	#endif
 
 	MODULE_NAME,
-	NULL,                  /* Functions */
+	NULL,                   /* Functions */
 	PHP_MINIT(trogdor),
-	NULL,                  /* MSHUTDOWN */
-	NULL,                  /* RINIT */
-	NULL,                  /* RSHUTDOWN */
-	NULL,                  /* MINFO */
+	PHP_MSHUTDOWN(trogdor),
+	NULL,                   /* RINIT */
+	NULL,                   /* RSHUTDOWN */
+	PHP_MINFO(trogdor),
 
 	#if ZEND_MODULE_API_NO >= 20010901
 	MODULE_VERSION,
