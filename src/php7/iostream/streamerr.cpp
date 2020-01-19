c7a91ddb93eb97c671e0a04e8399147e6c358d7a
James Colannino <crankycyclops@gmail.com>
2020-01-19 21:11:28 +0800

    PHP 7 module: fixed PHPStreamErr so that it's safe for non-ZTS builds

--- src/php7/iostream/streamerr.cpp
+++ src/php7/iostream/streamerr.cpp
@@ -3,17 +3,27 @@
 
 void PHPStreamErr::flush() {
 
-	if (trogdor::Trogerr::ERROR == getErrorLevel()) {
-		zend_throw_exception(EXCEPTION_GLOBALS(baseException), getBufferStr().c_str(), 0);
-	}
+	#ifdef ZTS
 
-	else if (trogdor::Trogerr::WARNING == getErrorLevel()) {
-		php_error_docref(NULL, E_WARNING, getBufferStr().c_str());
-	}
+		if (trogdor::Trogerr::ERROR == getErrorLevel()) {
+			zend_throw_exception(EXCEPTION_GLOBALS(baseException), getBufferStr().c_str(), 0);
+		}
 
-	else if (trogdor::Trogerr::INFO == getErrorLevel()) {
-		php_error_docref(NULL, E_NOTICE, getBufferStr().c_str());
-	}
+		else if (trogdor::Trogerr::WARNING == getErrorLevel()) {
+			php_error_docref(NULL, E_WARNING, getBufferStr().c_str());
+		}
+
+		else if (trogdor::Trogerr::INFO == getErrorLevel()) {
+			php_error_docref(NULL, E_NOTICE, getBufferStr().c_str());
+		}
+
+	#else
+
+		// TODO: for non-ZTS builds, offer an option for appending errors to
+		// a log file (configurable via php.ini.)
+		std::cerr << errorLevelToStr(getErrorLevel()) << ": " + getBufferStr() << std::endl;
+
+	#endif
 }
 
 std::unique_ptr<trogdor::Trogerr> PHPStreamErr::copy() {
