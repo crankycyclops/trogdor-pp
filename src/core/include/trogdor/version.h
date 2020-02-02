66d7daee7bc92e96ab710d8defaef8be69bc59f8
James Colannino <crankycyclops@gmail.com>
2020-02-02 09:42:14 +0800

    added version information to core

---
+++ src/core/include/trogdor/version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+
+// Current version of the core library
+#define TROGDOR_VERSION_MAJOR 0
+#define TROGDOR_VERSION_MINOR 1
+#define TROGDOR_VERSION_PATCH 0
+
+
+#endif
