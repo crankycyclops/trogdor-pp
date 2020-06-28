f80c6251f21a734145f66acd7e45258fc8fedc8c
James Colannino <crankycyclops@gmail.com>
2020-06-28 12:01:56 -0700

    core: moved core wander logic out of the timer job and into Creature itself and wrote unit tests to validate the timer job

--- src/core/test/timer/jobs/respawn.cpp
+++ src/core/test/timer/jobs/respawn.cpp
@@ -1,6 +1,61 @@
 #include <doctest.h>
 
+#include <trogdor/entities/creature.h>
+#include <trogdor/timer/jobs/respawn.h>
+
+#include <trogdor/iostream/nullin.h>
+#include <trogdor/iostream/nullout.h>
+#include <trogdor/iostream/nullerr.h>
+
 
 TEST_SUITE("Being Respawn Timer Job (timer/jobs/respawn.cpp)") {
 
+	TEST_CASE("Being Respawn Timer Job (timer/jobs/respawn.cpp): Respawn dead guy") {
+
+		trogdor::Game mockGame(std::make_unique<trogdor::NullErr>());
+
+		trogdor::entity::Creature deadGuy(
+			&mockGame,
+			"dead_guy",
+			std::make_unique<trogdor::NullOut>(),
+			std::make_unique<trogdor::NullErr>()
+		);
+
+		// Until we set a max health, a Being is considered immortal
+		deadGuy.setHealth(10), deadGuy.setMaxHealth(10);
+		deadGuy.die();
+
+		CHECK(!deadGuy.isAlive());
+
+		trogdor::Timer mockTimer(&mockGame);
+		trogdor::RespawnTimerJob job(&mockGame, 1, 1, 1, &deadGuy);
+
+		// Resurrection!
+		job.execute();
+		CHECK(deadGuy.isAlive());
+	}
+
+	TEST_CASE("Being Respawn Timer Job (timer/jobs/respawn.cpp): Respawn guy who's already alive") {
+
+		trogdor::Game mockGame(std::make_unique<trogdor::NullErr>());
+
+		trogdor::entity::Creature deadGuy(
+			&mockGame,
+			"alive_guy",
+			std::make_unique<trogdor::NullOut>(),
+			std::make_unique<trogdor::NullErr>()
+		);
+
+		// Until we set a max health, a Being is considered immortal
+		deadGuy.setHealth(10), deadGuy.setMaxHealth(10);
+
+		CHECK(deadGuy.isAlive());
+
+		trogdor::Timer mockTimer(&mockGame);
+		trogdor::RespawnTimerJob job(&mockGame, 1, 1, 1, &deadGuy);
+
+		// Not necessary, but thanks anyway!
+		job.execute();
+		CHECK(deadGuy.isAlive());
+	}
 }
--- src/core/timer/jobs/wander.cpp
+++ src/core/timer/jobs/wander.cpp
@@ -1,5 +1,3 @@
-#include <random>
-
 #include <trogdor/entities/room.h>
 #include <trogdor/entities/creature.h>
 #include <trogdor/timer/jobs/wander.h>
@@ -12,50 +10,13 @@ namespace trogdor {
 
    void WanderTimerJob::execute() {
 
-      static std::random_device rd;
-      static std::mt19937 generator(rd());
-      static std::uniform_real_distribution<double> probabilityDist(0, 1);
-
-      // make sure wandering wasn't turned off
       if (!wanderer->getWanderEnabled()) {
          setExecutions(0);
          return;
       }
 
-      if (auto location = wanderer->getLocation().lock()) {
-
-         // make sure Creature isn't in some special non-Room Place with no
-         // connections
-         if (ENTITY_ROOM != location->getType()) {
-            return;
-         }
-
-         // creature considers moving or staying; which will he pick?
-         else if (probabilityDist(generator) > wanderer->getWanderLust()) {
-            return;
-         }
-
-         auto curLoc = std::static_pointer_cast<Room>(location);
-         size_t nConnections = curLoc->getNumConnections();
-
-         // don't do anything if Creature is stuck in a room with no exits
-         if (!nConnections) {
-            return;
-         }
-
-         // TODO: use AI to select destination more intelligently; currently random
-         // The whole rest of this function will change when we do...
-         std::uniform_int_distribution<int> connectionsDist(0, nConnections - 1);
-
-         std::shared_ptr<Room> connection = curLoc->getConnectionByIndex(
-            connectionsDist(generator)
-         )->getShared();
-
-         // Only proceed if the connection is to a Room that still exists (valid
-         // pointer)
-         if (connection) {
-            wanderer->gotoLocation(connection);
-         }
+      else {
+         wanderer->wander();
       }
 
       // if wander interval ever changes, we should make sure it's updated
