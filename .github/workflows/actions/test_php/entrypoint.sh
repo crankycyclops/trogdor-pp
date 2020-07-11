8e34cf58300762de57bda4c7531ed2d5ca3c93c5
James Colannino <crankycyclops@gmail.com>
2020-07-11 15:28:45 -0700

    hopefully the last fix I need to make to get my php unit test action to work

--- .github/workflows/actions/test_php/entrypoint.sh
+++ .github/workflows/actions/test_php/entrypoint.sh
@@ -8,38 +8,38 @@ cd src/php7/trogdord
 # Test build against PHP 7.2
 /usr/bin/phpize7.2 && ./configure && make
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit 1
 fi
 
 make clean && /usr/bin/phpize7.2 --clean
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit 1
 fi
 
 # Test build against PHP 7.3
 /usr/bin/phpize7.3 && ./configure && make
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit 1
 fi
 
 make clean && /usr/bin/phpize7.3 --clean
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit 1
 fi
 
 # Test build against PHP 7.4
 /usr/bin/phpize7.4 && ./configure && make
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit 1
 fi
 
 make clean && /usr/bin/phpize7.4 --clean
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit 1
 fi
