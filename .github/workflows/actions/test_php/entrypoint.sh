bfc9b160b60fd79fc70340088e2ee3a96e8414d3
James Colannino <crankycyclops@gmail.com>
2020-07-11 15:24:15 -0700

    still trying to make the php unit test script work

--- .github/workflows/actions/test_php/entrypoint.sh
+++ .github/workflows/actions/test_php/entrypoint.sh
@@ -7,12 +7,39 @@ cd src/php7/trogdord
 
 # Test build against PHP 7.2
 /usr/bin/phpize7.2 && ./configure && make
+
+if [ $? -ne 0 ];
+	exit 1
+fi
+
 make clean && /usr/bin/phpize7.2 --clean
+
+if [ $? -ne 0 ];
+	exit 1
+fi
 
 # Test build against PHP 7.3
 /usr/bin/phpize7.3 && ./configure && make
+
+if [ $? -ne 0 ];
+	exit 1
+fi
+
 make clean && /usr/bin/phpize7.3 --clean
+
+if [ $? -ne 0 ];
+	exit 1
+fi
 
 # Test build against PHP 7.4
 /usr/bin/phpize7.4 && ./configure && make
+
+if [ $? -ne 0 ];
+	exit 1
+fi
+
 make clean && /usr/bin/phpize7.4 --clean
+
+if [ $? -ne 0 ];
+	exit 1
+fi
