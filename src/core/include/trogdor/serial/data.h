b993be9e6d6076d72a3d5bd358dd53e286622284
James Colannino <crankycyclops@gmail.com>
2020-11-12 09:14:16 -0800

    core: added comment explaining why I have to implement Serializable instead of just utilizing the underlying STL container directly.

--- src/core/include/trogdor/serial/data.h
+++ src/core/include/trogdor/serial/data.h
@@ -9,6 +9,13 @@
 namespace trogdor::serial {
 
 
+   /*
+      Instead of creating a wrapper class around an unordered_map, I wanted to
+      use the STL container directly. But the typedefs don't work because it
+      ends up being recursive. If someone can tell me how to define these two
+      containers so that I don't need to implement Serializable as a wrapper
+      class around std::unordered_map, I would be most grateful.
+   */
    class Serializable;
 
    typedef std::variant<
