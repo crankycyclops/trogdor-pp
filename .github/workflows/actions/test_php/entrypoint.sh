addf7031208bba8e1fd1bfc1ad6d6b69c0e0db03
James Colannino <james@colannino.dev>
2021-02-05 13:15:28 -0800

    php/trogdord: updated GitHub testing workflow to include PHP tests and support for PHP 8.0

--- .github/workflows/actions/test_php/entrypoint.sh
+++ .github/workflows/actions/test_php/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export TEST_PHP_ARGS="-q"
+
 cd src/php7/trogdord
 
 # TODO: I haven't implemented unit tests yet, so for now, this is just a test to
@@ -7,6 +9,12 @@ cd src/php7/trogdord
 
 # Test build against PHP 7.2
 /usr/bin/phpize7.2 && ./configure && make
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+make test
 
 if [ $? -ne 0 ]; then
 	exit 1
@@ -25,6 +33,12 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
+make test
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
 make clean && /usr/bin/phpize7.3 --clean
 
 if [ $? -ne 0 ]; then
@@ -38,8 +52,34 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
+make test
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
 make clean && /usr/bin/phpize7.4 --clean
 
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+
+# Test build against PHP 8.0
+/usr/bin/phpize8.0 && ./configure && make
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+make test
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+make clean && /usr/bin/phpize8.0 --clean
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
