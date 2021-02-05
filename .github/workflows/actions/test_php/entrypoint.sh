0b5ef3f86d73f10ff700e7e2f7a3666f4cbfd78e
James Colannino <james@colannino.dev>
2021-02-05 15:33:18 -0800

    php/trogdord: added installation and running of trogdord instance so that PHP tests can run using it

--- .github/workflows/actions/test_php/entrypoint.sh
+++ .github/workflows/actions/test_php/entrypoint.sh
@@ -4,6 +4,9 @@ export TEST_PHP_ARGS="-q"
 
 cd src/php7/trogdord
 
+trogdord &
+$TROGDORD_PID=$!
+
 # Test build against PHP 7.2
 /usr/bin/phpize7.2 && ./configure && make
 
@@ -22,6 +25,11 @@ make clean && /usr/bin/phpize7.2 --clean
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+
+kill $TROGDORD_PID
+
+trogdord &
+$TROGDORD_PID=$!
 
 # Test build against PHP 7.3
 /usr/bin/phpize7.3 && ./configure && make
@@ -42,6 +50,11 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
+kill $TROGDORD_PID
+
+trogdord &
+$TROGDORD_PID=$!
+
 # Test build against PHP 7.4
 /usr/bin/phpize7.4 && ./configure && make
 
@@ -60,6 +73,11 @@ make clean && /usr/bin/phpize7.4 --clean
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+
+kill $TROGDORD_PID
+
+#trogdord &
+#$TROGDORD_PID=$!
 
 # Test build against PHP 8.0
 #/usr/bin/phpize8.0 && ./configure && make
@@ -80,3 +98,4 @@ fi
 #	exit 1
 #fi
 
+#kill $TROGDORD_PID
