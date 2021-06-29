4b65d562e7297a11b3fcda5c26c3d7314d1a4f12
James Colannino <james@colannino.dev>
2021-06-29 14:03:39 -0700

    trogdord: added template for unit tests that must be written for output::Driver and output::Local

---
+++ src/trogdord/test/io/output/driver.cpp
@@ -0,0 +1,8 @@
+#include <doctest.h>
+#include <trogdord/io/output/driver.h>
+
+
+TEST_SUITE("output::Driver (io/output/driver.cpp)") {
+
+    // TODO
+}
---
+++ src/trogdord/test/io/output/local.cpp
@@ -0,0 +1,8 @@
+#include <doctest.h>
+#include <trogdord/io/output/local.h>
+
+
+TEST_SUITE("output::Local (io/output/local.cpp)") {
+
+    // TODO
+}
