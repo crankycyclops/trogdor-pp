b3e41583e659137ef5e3ffd46e357af16d426431
James Colannino <james@colannino.dev>
2021-09-04 22:29:49 -0700

    core: wrote more unit tests for LuaState

--- src/core/test/lua/luastate.cpp
+++ src/core/test/lua/luastate.cpp
@@ -32,6 +32,8 @@ TEST_SUITE("LuaState (luastate.cpp)") {
             SANITY_CHECK
         );
 
+        CHECK(game.get() == L.getGame());
+
         // Because I'm only executing Lua states in a single thread and because
         // locking would make things more complicated, I'm not going to call
         // L.lock().
@@ -67,6 +69,8 @@ TEST_SUITE("LuaState (luastate.cpp)") {
         // function is carried over and that it passes.
         trogdor::LuaState LCopy(L);
 
+        CHECK(game.get() == LCopy.getGame());
+
         LCopy.call("sanityCheck");
         LCopy.execute(1);
 
@@ -75,7 +79,7 @@ TEST_SUITE("LuaState (luastate.cpp)") {
         }
     }
 
-	TEST_CASE("LuaState (luastate.cpp): Deserialization Constructor") {
+	TEST_CASE("LuaState (luastate.cpp): Deserialization Constructor and serialize()") {
 
         #ifndef CORE_UNIT_TEST_LUA_ROOT
             FAIL("CORE_UNIT_TEST_LUA_ROOT must be defined.");
@@ -98,6 +102,8 @@ TEST_SUITE("LuaState (luastate.cpp)") {
         auto serialized = L.serialize();
         trogdor::LuaState deserializedL(game.get(), *serialized);
 
+        CHECK(game.get() == deserializedL.getGame());
+
         // Like the copy constructor, the deserialized LuaState instance should
         // have all of the same scripts loaded.
         deserializedL.call("sanityCheck");
@@ -106,5 +112,128 @@ TEST_SUITE("LuaState (luastate.cpp)") {
         if (!deserializedL.getBoolean(0)) {
             FAIL(deserializedL.getLastErrorMsg());
         }
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): Assignment Operator") {
+
+        #ifndef CORE_UNIT_TEST_LUA_ROOT
+            FAIL("CORE_UNIT_TEST_LUA_ROOT must be defined.");
+        #endif
+
+        std::unique_ptr<trogdor::Game> game = std::make_unique<trogdor::Game>(
+            std::make_unique<trogdor::NullErr>()
+        );
+
+        trogdor::LuaState L(game.get());
+
+        // Load into the Lua state a simple script that will be used as a
+        // sanity check that returns 1 if anything is wrong and 0 if not.
+        L.loadScriptFromFile(
+            std::string(CORE_UNIT_TEST_LUA_ROOT) +
+            STD_FILESYSTEM::path::preferred_separator +
+            SANITY_CHECK
+        );
+
+        trogdor::LuaState LCopy(game.get());
+        LCopy = L;
+
+        CHECK(game.get() == LCopy.getGame());
+
+        LCopy.call("sanityCheck");
+        LCopy.execute(1);
+
+        if (!LCopy.getBoolean(0)) {
+            FAIL(L.getLastErrorMsg());
+        }
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): getGame()") {
+
+        #ifndef CORE_UNIT_TEST_LUA_ROOT
+            FAIL("CORE_UNIT_TEST_LUA_ROOT must be defined.");
+        #endif
+
+        std::unique_ptr<trogdor::Game> game = std::make_unique<trogdor::Game>(
+            std::make_unique<trogdor::NullErr>()
+        );
+
+        trogdor::LuaState L(game.get());
+
+        CHECK(game.get() == L.getGame());
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): luaL_register_wrapper()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): luaL_checkudata_ex()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): pushEntity()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): pushArray()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): pushTable()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): getLastErrorMsg()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): loadScriptFromFile()") {
+
+        // TODO: test both valid and invalid files (as well as non-existent files)
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): loadScriptFromString()") {
+
+        // TODO: test blank string, string with syntax errors, and string with valid Lua
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): All variants of pushArgument()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): pushNilArgument()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): getBoolean()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): getNumber()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): getString()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): prime()") {
+
+        // TODO
+    }
+
+	TEST_CASE("LuaState (luastate.cpp): call() and execute()") {
+
+        // TODO: test both defined and undefined functions
     }
 }
