d835f01edc81563b3ced584e6cea607115989e16
James Colannino <james@colannino.dev>
2022-02-20 23:15:46 -0800

    trogdord: automated unit test fix

--- .github/workflows/actions/test_trogdord/entrypoint.sh
+++ .github/workflows/actions/test_trogdord/entrypoint.sh
@@ -19,5 +19,31 @@ timeout -s SIGQUIT 10 sudo -u app ./test_trogdord
 
 # This last set of unit tests is testing trogdord's handling of environment
 # variable overrides, hence the super long string of variables :)
+export TROGDORD_PORT="trogdord_port"
+export TROGDORD_REUSE_ADDRESS="reuse_address"
+export TROGDORD_SEND_TCP_KEEPALIVE="send_keepalive"
+export TROGDORD_LISTEN_IPS="listen_ip"
+export TROGDORD_LOGTO="logto"
+export TROGDORD_INPUT_LISTENERS="input_listen"
+export TROGDORD_OUTPUT_DRIVER="out_drive"
+export TROGDORD_REDIS_HOST="redis_host"
+export TROGDORD_REDIS_USERNAME="redis_username"
+export TROGDORD_REDIS_PASSWORD="redis_password"
+export TROGDORD_REDIS_PORT="redis_port"
+export TROGDORD_REDIS_CONNECTION_TIMEOUT="redis_timeout"
+export TROGDORD_REDIS_CONNECTION_RETRY_INTERVAL="redis_retry"
+export TROGDORD_REDIS_OUTPUT_CHANNEL="redis_output"
+export TROGDORD_REDIS_INPUT_CHANNEL="redis_input"
+export TROGDORD_DEFINITIONS_PATH="definitions_path"
+export TROGDORD_STATE_ENABLED="state_enable"
+export TROGDORD_STATE_AUTORESTORE_ENABLED="autorestore_enable"
+export TROGDORD_STATE_DUMP_SHUTDOWN_ENABLED="state_dump_shutdown"
+export TROGDORD_STATE_CRASH_RECOVERY_ENABLED="state_crash"
+export TROGDORD_STATE_FORMAT="state_format"
+export TROGDORD_STATE_PATH="state_path"
+export TROGDORD_STATE_MAX_DUMPS_PER_GAME="state_max_dumps"
+export TROGDORD_EXTENSIONS_PATH="ext_path"
+export TROGDORD_EXTENSIONS_LOAD="ext_load"
+
 make -j2 test_trogdord_envvar
-TROGDORD_PORT="trogdord_port" TROGDORD_REUSE_ADDRESS="reuse_address" TROGDORD_SEND_TCP_KEEPALIVE="send_keepalive" TROGDORD_LISTEN_IPS="listen_ip" TROGDORD_LOGTO="logto" TROGDORD_INPUT_LISTENERS="input_listen" TROGDORD_OUTPUT_DRIVER="out_drive" TROGDORD_REDIS_HOST="redis_host" TROGDORD_REDIS_USERNAME="redis_username" TROGDORD_REDIS_PASSWORD="redis_password" TROGDORD_REDIS_PORT="redis_port" TROGDORD_REDIS_CONNECTION_TIMEOUT="redis_timeout" TROGDORD_REDIS_CONNECTION_RETRY_INTERVAL="redis_retry" TROGDORD_REDIS_OUTPUT_CHANNEL="redis_output" TROGDORD_REDIS_INPUT_CHANNEL="redis_input" TROGDORD_DEFINITIONS_PATH="definitions_path" TROGDORD_STATE_ENABLED="state_enable" TROGDORD_STATE_AUTORESTORE_ENABLED="autorestore_enable" TROGDORD_STATE_DUMP_SHUTDOWN_ENABLED="state_dump_shutdown" TROGDORD_STATE_CRASH_RECOVERY_ENABLED="state_crash" TROGDORD_STATE_FORMAT="state_format" TROGDORD_STATE_PATH="state_path" TROGDORD_STATE_MAX_DUMPS_PER_GAME="state_max_dumps" TROGDORD_EXTENSIONS_PATH="ext_path" TROGDORD_EXTENSIONS_LOAD="ext_load" timeout -s SIGQUIT 10 sudo -u app ./test_trogdord_envvar
+timeout -s SIGQUIT 10 sudo -u app ./test_trogdord_envvar
